57a954667733502763591c59b2447c3de4e8276d
Elijah MacPherson <elijah.macpherson@gmail.com>
2021-04-30 17:29:40 -0400

    Implemented List<tildeInt>.sort()

---
+++ Types/tildeBool.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustAwful.Types
+{
+    public class tildeBool : tildeType<bool>
+    {
+        public tildeBool(bool value) : base(value) { }
+
+        public static tildeBool operator !(tildeBool s)
+        {
+            bool sv = getOne(s);
+            return new tildeBool(!sv);
+        }
+    }
+}
